f4d2133d7e244d28b9b8d64420ad406aa361c79f
codymaki <codymaki@live.com>
2021-01-12 12:12:42 -0600

    Initial commit

---
+++ src/main/java/com/bokudos/bokudosserver/BokudosServerApplication.java
@@ -0,0 +1,13 @@
+package com.bokudos.bokudosserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokudosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokudosServerApplication.class, args);
+	}
+
+}
