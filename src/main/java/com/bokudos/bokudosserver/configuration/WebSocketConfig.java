2155d686f3780a1b23b943f6c7a979ec75e38f77
codymaki <codymaki@live.com>
2021-01-14 17:41:41 -0600

    Add working socket code

--- src/main/java/com/bokudos/bokudosserver/configuration/WebSocketConfig.java
+++ src/main/java/com/bokudos/bokudosserver/configuration/WebSocketConfig.java
@@ -1,24 +1,38 @@
 package com.bokudos.bokudosserver.configuration;
 
+import com.bokudos.bokudosserver.sockets.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final static String ENDPOINT = "/chat";
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(getChatWebSocketHandler(), ENDPOINT)
+                .setAllowedOrigins("*");
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+    @Bean
+    public WebSocketHandler getChatWebSocketHandler() {
+        return new ChatWebSocketHandler();
     }
+
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/topic");
+//        config.setApplicationDestinationPrefixes("/app");
+//    }
+//
+//
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/chat");
+//        registry.addEndpoint("/chat").withSockJS();
+//    }
 }
---
+++ src/main/java/com/bokudos/bokudosserver/sockets/ChatWebSocketHandler.java
@@ -0,0 +1,43 @@
+package com.bokudos.bokudosserver.sockets;
+
+import com.bokudos.bokudosserver.data.Message;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class ChatWebSocketHandler extends AbstractWebSocketHandler {
+
+    private final List<WebSocketSession> webSocketSessions = new ArrayList<>();
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        log.info("Session Connected: " + session.getId());
+        webSocketSessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        Message messageObject = objectMapper.readValue(message.getPayload(), Message.class);
+        log.info("Message received: " + messageObject);
+        for(WebSocketSession webSocketSession: webSocketSessions) {
+            webSocketSession.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        log.info("Session removed: " + session.getId());
+        webSocketSessions.remove(session);
+    }
+}
