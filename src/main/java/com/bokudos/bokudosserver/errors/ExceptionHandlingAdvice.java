b2eb5353fa82c09416322ad527ef42910fa7974c
codymaki <codymaki@live.com>
2021-01-14 13:26:05 -0600

    Add exception handling advice

---
+++ src/main/java/com/bokudos/bokudosserver/errors/ExceptionHandlingAdvice.java
@@ -0,0 +1,39 @@
+package com.bokudos.bokudosserver.errors;
+
+import com.bokudos.bokudosserver.data.ErrorMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlingAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    public final ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), status);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+
+        String errorMessage = errors.stream()
+                .map(fieldError -> fieldError.getField() + " - " + fieldError.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining(", "));
+        return new ResponseEntity<>(new ErrorMessage(errorMessage), status);
+    }
+}
